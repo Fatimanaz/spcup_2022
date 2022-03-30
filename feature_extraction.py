63b48fda4a07b0d0617c73322371d250e627664d
Pulkit Mahajan <2019ucs0073@iitjammu.ac.in>
2022-03-30 11:32:52 +0530

    modified feature extraction to handle augmented dataset as well

--- feature_extraction.py
+++ feature_extraction.py
@@ -10,6 +10,7 @@ from collections import defaultdict
 import warnings
 warnings.filterwarnings("ignore")
 
+
 Main_folder = "./"
 train_data_path = Main_folder+"data/spcup_2022_training_part1/"
 eval_part1_path = Main_folder+"data/spcup_2022_eval_part1/"
@@ -28,28 +29,68 @@ def mfcc_simple_mean_feature(audio, sample_rate ):
     mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
     return mfccs_scaled_features
 
+def read_audio_clip(file_name):
+    audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
+    return audio, sample_rate
 
-metadata=pd.read_csv(train_data_path+"seen_unseen_labels.csv")
-file_list = metadata.track
-print("Number of files listed: ",len(file_list))
 
-feature_names=[ 'mfcc_simple_mean_new',"chroma_cqt_simple_mean_new"]#,  "chroma_cqt_simple_mean_new",  "melspectogram_mean_new"     ]
+metadata = pd.read_csv(train_data_path + 'labels.csv')
+file_list = metadata.track 
+labels = metadata.algorithm 
+
+noise_labels = defaultdict(list)
+reverb_labels = defaultdict(list)
+compressed_labels = defaultdict(list)
+
+for i in tqdm(range(len(file_list))):
+    file_name_prefix = file_list[i].split('.')[0]
+    noise_file_name = file_name_prefix + '_noise.wav'
+    noise_labels['track'].append(noise_file_name)
+    noise_labels['algorithm'].append(labels[i])
+    reverb_file_name = file_name_prefix + '_reverb.wav'
+    reverb_labels['track'].append(reverb_file_name)
+    reverb_labels['algorithm'].append(labels[i])
+    compressed_file_name = file_name_prefix + '_compressed.mp3'
+    compressed_labels['track'].append(compressed_file_name)
+    compressed_labels['algorithm'].append(labels[i])
+
+
+df = pd.DataFrame(noise_labels)
+print(df.head())
+noise_labels_filepath=Path( "./data/spcup_2022_training_part1_noise_added/"+"labels.csv")  
+noise_labels_filepath.parent.mkdir(parents=True, exist_ok=True) 
+df.to_csv(noise_labels_filepath)  
+df = pd.DataFrame(reverb_labels)
+print(df.head())
+noise_labels_filepath=Path("./data/spcup_2022_training_part1_reverb_added/" +"labels.csv")  
+noise_labels_filepath.parent.mkdir(parents=True, exist_ok=True) 
+df.to_csv(noise_labels_filepath) 
+df = pd.DataFrame(compressed_labels)
+print(df.head())
+noise_labels_filepath=Path("./data/spcup_2022_training_part1_compressed/" +"labels.csv")  
+noise_labels_filepath.parent.mkdir(parents=True, exist_ok=True) 
+df.to_csv(noise_labels_filepath) 
+
 
 feature_storage_folder = Main_folder+'ExtractedFeatures/'
 if not os.path.exists(feature_storage_folder):
     os.makedirs(feature_storage_folder)
 
-def read_audio_clip(file_name):
-    audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
-    return audio, sample_rate
+data_folders = os.listdir('./data')
+for folder in data_folders:
 
-# for every folder now and then
-feature_storage_subfolders = ['train_part1', 'eval_part1', 'eval_part2']
-for subfolder_name in feature_storage_subfolders:
+    metadata = pd.read_csv(f'./data/{folder}/labels.csv')
+    print("read data successfully")
+    file_list = metadata.track
+    print(len(file_list))
+
+    feature_names=[ 'mfcc_simple_mean_new',"chroma_cqt_simple_mean_new"]
+
+
     Features = defaultdict(list)  
 
     for i in tqdm(range(len(file_list))):
-        file_name = train_data_path + file_list[i]
+        file_name = f'./data/{folder}/' + file_list[i]
         audio, sr = read_audio_clip(file_name) 
 
         Features['mfcc_simple_mean_new'].append([ mfcc_simple_mean_feature(audio, sr )])
@@ -57,19 +98,21 @@ for subfolder_name in feature_storage_subfolders:
     
 
     # storing the extracted feaures into a folder
-    subfolder = feature_storage_folder + subfolder_name+ "/"
+    subfolder = feature_storage_folder + folder+ "/"
     if not os.path.exists(subfolder):
         os.makedirs(subfolder)
 
     extracted_features_mfcc_df=pd.DataFrame(Features['mfcc_simple_mean_new'],columns=['feature'])
     feature_list = extracted_features_mfcc_df['feature'].tolist()
     df = pd.DataFrame(feature_list)
-    feature_filepath = Path(subfolder + f"mfcc_features_{subfolder_name}.csv")  
+    feature_filepath = Path(subfolder + f"mfcc_features.csv")  
     # feature_filepath.parent.mkdir(parents=True, exist_ok=True) 
     df.to_csv(feature_filepath)  
+    print(df.head())
 
     extracted_features_chroma_df = pd.DataFrame(Features['chroma_cqt_simple_mean_new'], columns = ['feature'])
     feature_list = extracted_features_chroma_df['feature'].tolist()
     df = pd.DataFrame(feature_list)
-    feature_filepath = Path(subfolder + f"chroma_cqt_features_{subfolder_name}.csv")
+    feature_filepath = Path(subfolder + f"chroma_cqt_features.csv")
     df.to_csv(feature_filepath)
+    print(df.head())
