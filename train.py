f82400cf767fc88946c1a10e70cd02790874f1fe
Pulkit Mahajan <2019ucs0073@iitjammu.ac.in>
2022-03-30 21:40:18 +0530

    train.py complete

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ from datetime import datetime
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
 from sklearn import svm
@@ -61,5 +62,27 @@ df = pd.concat([df1, df2, df3, df4], axis = 0)
 
 print(f"{df.shape}: Features queried")
 
+y_singleNode = np.array(df['class'])
+X = np.array(df.drop(['class'],axis=1))
+print(X.shape)
+print(y_singleNode.shape)
 
+X_train, X_val, y_train, y_val = train_test_split(X, y_singleNode, test_size=0.25, random_state=42, stratify=y_singleNode)
+model = svm.SVC(kernel = 'linear', C = 4, probability = True).fit(X, y_singleNode)
+results = model.score(X_val, y_val)
+print(results) 
 
+num_classes=6
+y_val_predict = model.predict(X_val)
+Confusion_Matrix = confusion_matrix(y_val, y_val_predict, labels = [0,1,2,3,4,5])
+print(Confusion_Matrix)
+
+Model_Name=str(model).replace(",","_").replace(" ", "_").replace(")", "_").replace("(", "_").replace("'", "").replace("=","_")
+print(Model_Name)
+MODEL_SAVE_PATH = './Results/svm_on_mfcc_and_chroma_cqt_' + Model_Name + "/"
+print(MODEL_SAVE_PATH)
+
+if not os.path.exists(MODEL_SAVE_PATH):
+    os.makedirs(MODEL_SAVE_PATH)
+
+pickle.dump(model, open(MODEL_SAVE_PATH + "SavedModel.sav", 'wb'))
